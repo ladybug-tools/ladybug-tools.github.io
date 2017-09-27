b6ac3c6dcee16a485eda437d4ce6862d91c0d868
Chris Mackey <chris@mackeyarchitecture.com>
2017-09-26 21:23:58 -0400

    Adding Python Script to Update Navigation

---
+++ eval/assets/py/updateNav.py
@@ -0,0 +1,35 @@
+import os
+
+# List of files that should not be updated by this script.
+skipFiles = ['nav.html', 'index.html']
+
+# Read the directory of the nav.html
+currentDir = os.getcwd()
+navFileAddress = currentDir + '//nav.html'
+with open(navFileAddress, 'r') as navFile:
+    navStr = navFile.read()
+
+# Replace the headers in all of the html files.
+for htmlFile in os.listdir(currentDir):
+    if htmlFile.endswith('.html') and htmlFile not in skipFiles:
+        htmlFileAddress = currentDir + '//' + htmlFile
+        searchStr = ''
+        navTrigger = False
+        with open(htmlFileAddress, 'r') as webPageFile:
+            # Find the text to replace
+            for line in webPageFile:
+                if line.startswith('  <nav'):
+                    navTrigger = True
+                    searchStr = searchStr + line
+                elif line.startswith('  </nav>'):
+                    navTrigger = False
+                    searchStr = searchStr + line
+                elif navTrigger == True:
+                    searchStr = searchStr + line
+
+            # Replace the text.
+            text = webPageFile.read()
+            text = text.replace(searchStr, navStr)
+
+        with open(htmlFileAddress, 'w') as f:
+          f.write(text)
